6a26f0966b397c7368bdbe472ea83889f018bc24
vansh-cryp <vanshgusain920@gmail.com>
2025-10-07 00:11:03 +0530

    feat: initialize VS Code extension with basic structure and testing setup
    - Add package.json for extension configuration and dependencies
    - Create initial test suite for extension functionality
    - Include quickstart guide for extension development in vsc-extension-quickstart.md

---
+++ extension.js
@@ -0,0 +1,30 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('codify.helloWorld', 
+		function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from Codify!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
